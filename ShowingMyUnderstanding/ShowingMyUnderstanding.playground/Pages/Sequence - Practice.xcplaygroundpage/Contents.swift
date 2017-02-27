dd0a8be2092ed3e2ccd229120f886d5c9a9c3797
Student <student@mac-book-angus-mcleish.local>
2017-02-27 13:03:40 -0500

    I finished the M

--- ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Sequence - Practice.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Sequence - Practice.xcplaygroundpage/Contents.swift
@@ -29,15 +29,19 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
 
+//Set left side colour
+canvas.lineColor = Color(hue: 200, saturation: 82, brightness: 100, alpha: 75)
 
-canvas.lineColor = Color.blue
-//canvas.lineColor = Color(hue: 300, saturation: 300, brightness: 300, alpha: 300)
-//left line
+//left lines
 canvas.drawLine(fromX: 75, fromY: 75, toX: 75, toY: 230, lineWidth: 75)
+canvas.drawLine(fromX: 75, fromY: 230, toX: 150, toY: 75, lineWidth: 75)
 
-//Right line
+
+//Set right side colour
+canvas.lineColor = Color(hue: 0, saturation: 82, brightness: 100, alpha: 75)
+//Right lines
 canvas.drawLine(fromX: 225, fromY: 75, toX: 225, toY: 230, lineWidth: 75)
-canvas.drawEllipse(centreX: <#T##Int#>, centreY: <#T##Int#>, width: 75, height: 75)
+canvas.drawLine(fromX: 225, fromY: 230, toX: 150, toY: 75, lineWidth: 75)
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
